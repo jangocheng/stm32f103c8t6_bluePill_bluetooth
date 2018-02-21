936929f6c50ef532dd567e350f255d573d4546c3
linuxgg <qdtom@foxmail.com>
2018-02-21 18:39:55 +0800

    led ok

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32f103c8t6_bluePill_bluePill' 
+ * Target:  'stm32f103c8t6_bluePill_bluePill' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
